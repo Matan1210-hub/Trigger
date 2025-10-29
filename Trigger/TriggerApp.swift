4105ed9268a1eb0022c8ba162cac7d5deca74f04
matan <matanbcohen@gmail.com>
2025-10-29 18:18:10 +0200

    Initial Commit

---
+++ Trigger/TriggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriggerApp.swift
+//  Trigger
+//
+//  Created by Matan Cohen on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct TriggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriggerTests/TriggerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TriggerTests.swift
+//  TriggerTests
+//
+//  Created by Matan Cohen on 29/10/2025.
+//
+
+import Testing
+
+struct TriggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
